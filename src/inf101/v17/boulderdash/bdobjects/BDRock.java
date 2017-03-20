bca82af3b9c8f5f2c48a73dbf95561ff615ce7e6
milli <milli@EPICKATT>
2017-03-20 16:32:48 +0100

    Added push method

--- src/inf101/v17/boulderdash/bdobjects/BDRock.java
+++ src/inf101/v17/boulderdash/bdobjects/BDRock.java
@@ -1,6 +1,7 @@
 package inf101.v17.boulderdash.bdobjects;
 
 import inf101.v17.boulderdash.Direction;
+import inf101.v17.boulderdash.IllegalMoveException;
 import inf101.v17.boulderdash.Position;
 import inf101.v17.boulderdash.maps.BDMap;
 import javafx.scene.paint.Color;
@@ -11,38 +12,46 @@ public class BDRock extends AbstractBDFallingObject {
 
 	public BDRock(BDMap owner) {
 		super(owner);
-		// TODO Auto-generated constructor stub
+
 	}
 
 	@Override
 	public Paint getColor() {
-		// TODO Auto-generated method stub
+
 		return Color.LIGHTGRAY;
 	}
 
 	public boolean push(Direction dir) {
-		try {
-			if (dir == Direction.WEST || dir == Direction.EAST) {
-				Position rockPos = this.getPosition();
-				Position next = (Position) owner.get(nextPos);
-				if (owner.canGo(nextPos) && owner.get(nextPos) instanceof BDEmpty) {
+		Position rockPos = this.getPosition();
 
-					prepareMove(nextPos);
-					if (!(rockPos == next)) {
+		switch (dir) {
+		case EAST:
+			if (owner.canGo(rockPos, dir)) {
+				if (owner.get(rockPos.getX() + 1, rockPos.getY()) instanceof BDEmpty) {
+					try {
+						prepareMove(rockPos.getX() + 1, rockPos.getY());
+						step();
 						return true;
+					} catch (IllegalMoveException e) {
+						return false;
 					}
 				}
-
 			}
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
-
+		case WEST:
+			if (owner.canGo(rockPos, dir)) {
+				if (owner.get(rockPos.getX() - 1, rockPos.getY()) instanceof BDEmpty) {
+					try {
+						prepareMove(rockPos.getX() - 1, rockPos.getY());
+						step();
+						return true;
+					} catch (IllegalMoveException e) {
+						return false;
+					}
+				}
+			}
+		default:
+			return false;
 		}
-
-		return false;
-
 	}
 
 }
