779e4e94fe097ff7226ffbd072b2fdf2bdd3bf94
milli <milli@EPICKATT>
2017-03-21 01:28:24 +0100

    Added future tests - have not yet to be implemented

--- src/inf101/v17/boulderdash/bdobjects/tests/PlayerTest.java
+++ src/inf101/v17/boulderdash/bdobjects/tests/PlayerTest.java
@@ -1,5 +1,84 @@
 package inf101.v17.boulderdash.bdobjects.tests;
 
-public class PlayerTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import org.junit.Test;
+
+import inf101.v17.boulderdash.Position;
+import inf101.v17.boulderdash.bdobjects.BDBug;
+import inf101.v17.boulderdash.bdobjects.IBDObject;
+import inf101.v17.boulderdash.maps.BDMap;
+import inf101.v17.datastructures.IGrid;
+import inf101.v17.datastructures.MyGrid;
+
+public class PlayerTest {
+	
+	private BDMap map;
+	
+	@Test
+	public void diamondTest() {
+		IGrid<Character> grid = new MyGrid<>(3, 3, ' ');
+		grid.set(0, 0, '*');
+		grid.set(0, 1, '*');
+		grid.set(0, 2, '*');
+		grid.set(1, 0, '*');
+		grid.set(1, 2, '*');
+		grid.set(2, 0, '*');
+		grid.set(2, 1, '*');
+		grid.set(2, 2, '*');
+		grid.set(1, 1, 'b');
+		map = new BDMap(grid);
+
+		// find the bug
+		Position bugPos = new Position(1, 1);
+		IBDObject bug = map.get(bugPos);
+
+		assertTrue(bug instanceof BDBug);
+
+		for (int i = 0; i < 10; i++) {
+			map.step();
+			if (map.get(bugPos) != bug) {
+
+				fail("Bug is not enclosed");
+
+			}
+
+		}
+
+		assertEquals(bugPos, map.getPosition(bug));
+	}
+	
+	public void pushTest() {
+		IGrid<Character> grid = new MyGrid<>(3, 3, ' ');
+		grid.set(0, 0, '*');
+		grid.set(0, 1, '*');
+		grid.set(0, 2, '*');
+		grid.set(1, 0, '*');
+		grid.set(1, 2, '*');
+		grid.set(2, 0, '*');
+		grid.set(2, 1, '*');
+		grid.set(2, 2, '*');
+		grid.set(1, 1, 'b');
+		map = new BDMap(grid);
+
+		// find the bug
+		Position bugPos = new Position(1, 1);
+		IBDObject bug = map.get(bugPos);
+
+		assertTrue(bug instanceof BDBug);
+
+		for (int i = 0; i < 10; i++) {
+			map.step();
+			if (map.get(bugPos) != bug) {
+
+				fail("Bug is not enclosed");
+
+			}
+
+		}
+
+		assertEquals(bugPos, map.getPosition(bug));
+	}
 }
